56f932165e2b272d84d03956fcb9cf11a00845d7
Harold Ship <harold.ship@gmail.com>
2017-08-21 09:19:13 +0300

    convergence for MISE vs. #cases

---
+++ results/by_num_cases/by_num_cases.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+
+setwd("/Users/harold/Dropbox/MA_Knowledge_and_Info/Thesis/thesis/results/by_num_cases")
+
+Cases <- c(50, 100, 200, 500, 1000)
+
+dfO <- data.frame(Case=Cases,
+                  Bandwidth="Oracle",
+                  MISE=c(0.000008, 0.000022, 0.000053, 0.000070, 0.000379),
+                  RMISE=c(0.005208, 0.003358, 0.002029, 0.001041, 0.000581)
+                  )
+dfS <- data.frame(Case=Cases,
+                  Bandwidth="Silverman",
+                  MISE=c(0.000014, 0.000035, 0.000083, 0.000265, 0.000619),
+                  RMISE=c(0.008600, 0.005293, 0.003184, 0.001626, 0.000948)
+                  )
+dfC <- data.frame(Case=Cases,
+                  Bandwidth="CV",
+                  MISE=c(0.000014, 0.000034, 0.000082, 0.000244, 0.000484),
+                  RMISE=c(0.008580, 0.005266, 0.003129, 0.001496, 0.000742)
+                  )
+df <- rbind(dfO, dfS, dfC)
+df$Bandwidth <- as.factor(df$Bandwidth)
+
+pdf(file="MISE-vs-cases.pdf")
+ggplot(df) +
+  geom_line(aes(x=Case, y=MISE, colour=Bandwidth))
+dev.off()
+pdf(file="RMISE-vs-cases.pdf")
+ggplot(df) +
+  geom_line(aes(x=Case, y=RMISE, colour=Bandwidth))
+dev.off()
+
