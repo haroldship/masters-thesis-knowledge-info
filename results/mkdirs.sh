0520e78d5f1af74cb57bf94eba7a02bcb43d5545
Harold Ship <harold.ship@gmail.com>
2018-01-15 21:48:58 +0200

    big update of many results

--- results/mkdirs.sh
+++ results/mkdirs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-DECAYS="unif 2.0 1.4 1 0.7"
+DECAYS="1.0"
 INCIDENTS="50 100 200 500 1000"
 
 for D in $DECAYS; do
   for I in $INCIDENTS; do
     dstr="unif_${I}_${D}_1h"
-    [ -d $dstr ] && continue;
-    echo "making directory $dstr"
-    cp -a template $dstr
+#    [ -d $dstr ] && continue;
+#    [ -d $dstr ] && echo "removing $dstr";
+    [ -d $dstr ] && rm -rf $dstr/experiment* $dstr/template;
+    echo "preparing $dstr"
+    cp -a template/* $dstr/
     if [ $D = "unif" ]; then
       sed -e "s/genhill(sigma=1.0)/genunif()/" -e "s/EN.i=50/EN.i=${I}/" -i "bak" $dstr/experiment_setup.R
     else
